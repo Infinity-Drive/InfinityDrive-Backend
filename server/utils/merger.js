6fa47da693639be1cf58531c03b75d5d5cc58c4b
mhd3v <mamir3012@gmail.com>
2019-03-21 21:28:02 +0500

    Change merger to be dynamic

--- server/utils/merger.js
+++ server/utils/merger.js
@@ -1,57 +1,37 @@
-const fs = require('fs');
+const merger = (readStreams, response) => new Promise((resolve, reject) => {
+  const streamCount = readStreams.length;
+  // need a separate index for current stream being read, for loop counter would always point
+  // to last indexsince for loop runs completely at first, registering all the listeners
+  let currentStreamIndex = 0;
 
-// var stats = fs.statSync("a.rar");
-// var fileSizeInBytes = stats["size"];
-// var streamSize = Math.ceil(fileSizeInBytes / 3);
-const readStream = fs.createReadStream('file0.bin');
-const readStream1 = fs.createReadStream('file1.bin');
-const readStream2 = fs.createReadStream('file2.bin');
+  // iterate over all streams to register end listeners
+  for (let index = 0; index < streamCount; index += 1) {
+    const stream = readStreams[index];
 
-const dest = fs.createWriteStream('b.mkv');
+    // set end listeners for all streams apart from the last stream
+    if (index !== streamCount - 1) {
+      stream.on('end', () => {
+        readStreams[currentStreamIndex].unpipe(response);
+        currentStreamIndex += 1;
+        // check if readStream isn't the second last stream
+        if (currentStreamIndex !== streamCount - 2) {
+          readStreams[currentStreamIndex].pipe(response, { end: false });
+        }
+        else {
+          readStreams[currentStreamIndex].pipe(response);
+        }
+      });
+    }
+    // last stream end listener; want to resolve once last stream is read
+    else {
+      stream.on('end', () => {
+        resolve();
+      });
+    }
+  }
 
-
-// var Promises = require('promise');
-const promise = new Promise(((resolve, reject) => {
-  // do some async stuff
-
-  readStream.pipe(dest, { end: false });
-  readStream.on('end', () => {
-    readStream.unpipe(dest);
-    resolve('ab');
-  });
-  console.log('1');
-}));
-promise.then(data => new Promise(((resolve, reject) => {
-  // second async stuff
-
-  readStream1.pipe(dest, { end: false });
-  readStream1.on('end', () => {
-    resolve('ab');
-  });
-  console.log('2');
-})),
-(reason) => {
-  // error handler
-  console.log(reason);
-}).then((data) => {
-  // second success handler
-  readStream2.pipe(dest);
-
-  readStream2.on('end', () => {
-    dest.end();
-  });
-  console.log('3');
-}, (reason) => {
-  console.log(reason);
-  // second error handler
+  // start piping from first read stream
+  readStreams[0].pipe(response, { end: false });
 });
 
-
-// var counter = 0;
-
-// readStream.on('data', (chunk) => {
-//     var writeStream = fs.createWriteStream(`file${counter}`);
-//     writeStream.write(chunk);
-//     counter++;
-//     console.log("hello");
-// });
+module.exports = { merger };
